9c0fe15efbb4e43cdc825c2862d4bc35e9b1d838
clay-most <46730958+clay-most@users.noreply.github.com>
2019-07-14 16:38:31 -0600

    build reducers

---
+++ todo/src/reducer.js
@@ -0,0 +1,34 @@
+import { ADD_TASK, TOGGLE_TASK } from './actions';
+
+const initailState = {
+  tasks: []
+};
+
+export const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          { id: Date.now(), value: action.payload, comopleted: false }
+        ]
+      };
+    case TOGGLE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.map(item => {
+          if (item.id === action.payload) {
+            return {
+              ...item,
+              completed: !item.completed
+            };
+          } else {
+            return item;
+          }
+        })
+      };
+    default:
+      return state;
+  }
+};
